e6929203c520a4f5b48bfd679c090a7dc6561e22
Aaron Strong <awstrong@gmail.com>
2018-11-29 19:51:12 -0600

    Add more functions

---
+++ DownloadXenDesktopIso.ps1
@@ -0,0 +1,162 @@
+﻿<#   
+
+Copyright © 2015-2016 Citrix Systems, Inc. All rights reserved.
+
+.SYNOPSIS
+Download a file from the specified location and optional verify download using hash
+
+.DESCRIPTION
+This script will download a file from the specified location to the
+specified destination folder. If there is a file of name $FileName.<Algorithm> in the
+same location then we assume this contains a checksum which will also be downloaded
+and used to verify the download succeeded. Valud values for <Algorithm> are SHA256, MD5
+
+
+.PARAMETER FileName
+The name of the file to retrieve
+
+.PARAMETER Path
+The location to copy the file from. This may be 
+    An HTTP(S) URL (e.g. https://example.com/downloads), 
+    A CIFS share (e.g. \\computer\downloads) or
+    A simple file system path (e.g. D:\downloads)
+
+.PARAMETER ToFolder
+Destination folder for the file.
+
+.PARAMETER CifsUser
+Optional user name for accessing CIFS share
+
+.PARAMETER CifsPassword
+Password for CIFS user
+
+.OUTPUTS
+On success the script returns the local path of the downloaded file.
+
+#>
+[CmdletBinding()]
+Param (
+    [string]$FileName,
+    [string]$Path,
+    [string]$ToFolder = "$Env:Temp",
+    [string]$CifsUser,
+    [string]$CifsPassword
+)
+
+# Download a file from the web with a retry loop using dlup.exe for preference; if dlup is
+# not available then the .NET WebClient will be used.
+function Download-WebFile {
+    Param (
+        [string]$url,
+        [string]$localFile,
+        [int]$retries = 2
+    )
+    $dlup = "..\..\dlup.exe"
+    if (Test-Path -Path $dlup) {
+        Write-Verbose "Download using dlup"
+        $out = ".\dlup.out"
+        $err = ".\dlup.err"
+        try { 
+            Write-Verbose "Starting download of $url (will retry $retries times on failure)"
+            $proc = Start-Process -FilePath $dlup -ArgumentList "-p $url -o $localFile -r $retries" -Wait -NoNewWindow -PassThru `
+                -RedirectStandardError $err -RedirectStandardOutput $out
+            if ($proc.ExitCode -ne 0) {               
+                throw "DLUP download failed with exit code: $($proc.ExitCode) `n$(Get-Content $err) `n$(Get-Content $out)"
+            }
+        } finally {
+            Remove-Item $out
+            Remove-Item $err
+        }
+
+    } else {
+        Write-Verbose "Download using .NET WebClient"
+        $ex = ""
+        for ($i=0; $i -lt $retries; $i++) {
+            try {
+                $client = New-Object System.Net.WebClient
+                 Write-Verbose "Starting download of $url [attempt $i]"
+                $client.DownloadFile($url, $localFile)
+                return
+            } catch {
+                Write-Verbose "Exception during download $_"
+                $ex = $_
+                Start-Sleep -Seconds 5
+            }
+        }
+        throw $ex
+    }
+}
+
+function Download-File {
+    Param (
+        [string]$FileName,
+        [string]$Path,
+        [string]$ToFolder = "$Env:Temp"
+    )
+    if (-not (Test-Path $ToFolder)) {
+        New-Item -Path $ToFolder -ItemType Directory | Out-Null
+    }  
+    if ($ToFolder -eq ".") {
+        $ToFolder = Get-Location
+    }
+    $localFile = Join-Path "$ToFolder" "$FileName"
+    if (-not $Path) {
+        $Path = ".\"
+    }
+    if ($Path -match "^https?://\w+") {
+        # Web URL
+        $Url = "$Path/$FileName"
+        Download-WebFile $url $localFile
+    } elseif (($Path -match "^\\\\\w+") -or ($Path -match "^[C-Z]:\\\w*") -or ($Path -match "^.\\\w*")) {
+        # CIFS file share or local path
+        if ($Path.EndsWith('\')) {
+            $Path = $Path.TrimEnd('\')
+        }
+        if (-not [string]::IsNullOrEmpty($CifsUser)) {
+            net use $Path "$CifsPassword" /user:$CifsUser | Out-Null
+            if ($LastExitCode -ne 0) {
+                throw "Unable to connect to share $Path using the given credentials: $CifsUser"
+            }
+        }
+        Copy-Item -Path "$Path\$FileName" -Destination $ToFolder
+        if (-not [string]::IsNullOrEmpty($CifsUser)) {
+            net use $Path /delete | Out-Null
+        }
+    } else {
+        throw "Cannot understand download path: $Path"
+    }
+    return $localFile
+}
+
+$ErrorActionPreference = "Stop"
+
+try {
+    # In order of preference
+    $HashAlgorithms = @("SHA256", "MD5")
+
+    # Download the principal file
+    $downloadedFile = Download-File -FileName $FileName -Path $Path -ToFolder $ToFolder
+
+    # Look for files containing hash
+    foreach ($HashAlgorithm in $HashAlgorithms) {
+        $hashFile = ""
+        try { 
+            $hashFile = Download-File -FileName "$FileName.$HashAlgorithm" -Path $Path -ToFolder $ToFolder
+        } catch {
+             Write-Verbose "$FileName.$HashAlgorithm not found"
+             continue
+        }
+        $Hash = Get-Content -Path $hashFile
+        Write-Verbose "Calculating $HashAlgorithm hash of $FileName"
+        $fileHash = Get-FileHash -Path $downloadedFile -Algorithm $HashAlgorithm
+        if ($fileHash.Hash.ToUpper() -eq $Hash.ToUpper()) {
+            break
+        }
+        throw "Download checksum failed. File hash is $fileHash, expected $Hash"       
+    }
+    $downloadedFile
+
+} catch {
+    $_
+    exit 1
+}
---
+++ InstallDotNet.ps1
@@ -0,0 +1,101 @@
+﻿<#
+    
+Copyright © 2017 Citrix Systems, Inc. All rights reserved.
+
+.SYNOPSIS
+Install required version of .NET Framework from XenDesktop ISO
+
+.DESCRIPTION
+The script will mount the specified ISO using native PowerShell cmdlet and take a look to see if
+one of the designated .NET installers is there; if so it wil be installed.
+
+If none of the designated installers can be found the script will check that the appropriate .NET
+Windows feature is installed.
+
+.PARAMETER IsoPath
+The path to the ISO
+
+#>
+#Requires -Version 3
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory=$true)]
+    [string]$IsoPath
+)
+
+function Mount-ISO {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string]$IsoPath,
+        [int]$Retries = 3
+    )
+    for ($i = 1; $i -le $Retries; $i++) {
+        $image = Mount-DiskImage -ImagePath $IsoPath -StorageType ISO -PassThru -ErrorAction Stop
+        if ($image) {
+            $volume = $image | Get-Volume
+            if ($volume) {
+                return $volume.DriveLetter + ":"
+            } else {
+                Write-Warning "Failed to get volume from image (attempt $i of $Retries)"
+            }
+        } else {
+            Write-Warning "Failed to mount disk image $IsoPath (attempt $i of $Retries)"
+        }
+        Start-Sleep -Seconds 5
+    }
+    throw "Unable to mount image at $IsoPath"
+}
+
+function Add-DotNetFeature {
+    [CmdletBinding()]
+    Param()
+   
+    Import-Module ServerManager
+    foreach ($name in @("NET-Framework-45-Features", "AS-NET-Framework")) {
+        $feature = Get-WindowsFeature -Name $name -ErrorAction SilentlyContinue
+        if ($feature -and ($feature.InstallState -eq "Available")) {
+            Write-Host "Adding feature $name"
+            Add-WindowsFeature -Name $name -IncludeAllSubFeature
+        }
+    }
+}
+
+$ErrorActionPreference = 'Stop'
+try {
+    $drive = Mount-ISO -IsoPath $IsoPath    
+    Write-Host "Looking for .NET insallers on drive $drive"
+    # Possible .NET installers in order of preference
+    $dotNetInstallers = @(
+        (New-Object PSObject -Property @{ Name = ".NET 4.5.2"; Path = "Support\DotNet452\NDP452-KB2901907-x86-x64-AllOS-ENU.exe"}),
+        (New-Object PSObject -Property @{ Name = ".NET 4.5.1"; Path = "Support\DotNet451\NDP451-KB2858728-x86-x64-AllOS-ENU.exe"})
+    )
+
+    foreach ($entry in $dotNetInstallers) {
+
+        $installer = Join-Path -Path $drive -ChildPath $entry.Path
+        $name = $entry.Name
+        if (Test-Path $installer) {
+            Write-Host "Found installer $installer"
+            $argList = "/q /norestart"
+            $proc = Start-Process -FilePath $installer -ArgumentList $argList -Wait -NoNewWindow -PassThru
+            if (($proc.ExitCode -eq 3) -or ($proc.ExitCode -eq 3010)) {
+                return "$name installed. Reboot required"
+            } elseif ($proc.ExitCode -ne 0) {
+                throw "$installer $argList failed: error code $($proc.ExitCode)"
+            }
+            return "$name installed"
+        } else {
+            Write-Warning "$installer not found on $drive drive ($IsoPath)"
+        }
+    }
+    Write-Warning "No .NET installer found on path $IsoPath"
+    Add-DotNetFeature
+    
+} catch {
+    $_
+    exit 1
+} finally {
+    if (Test-Path $IsoPath) {
+        Dismount-DiskImage -ImagePath $IsoPath
+    }
+}
